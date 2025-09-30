f0bfcee078086fcda55e1b524ce495c41736a477
dazzlingcomputer <118881048+dazzlingcomputer@users.noreply.github.com>
2025-09-30 21:01:37 +0800

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{textAlign: 'center', marginTop: '20%'}}>
-      <h1>LMAchat 正在加载 lmarena.ai...</h1>
-      <meta httpEquiv="refresh" content="0; url=/api/" />
-    </div>
-  );
-}
